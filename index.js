778e5208c90902b8df0418b457c892f75a5bf807
poojalohmod <poojalohmod3@gmail.com>
2026-01-09 18:41:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./App.css";
+// Import boxen
+const boxen = require("boxen");
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+// Define message and title
+const message = "I am using my first external module!";
+const title = "Hurray!!!";
+
+// Classic (default style)
+const classicBox = boxen(message, {
+  title: title,
+  titleAlignment: "center",
+  padding: 1,
+  margin: 1,
+  borderStyle: "classic"
+});
+
+// SingleDouble (mixed single and double borders)
+const singleDoubleBox = boxen(message, {
+  title: title,
+  titleAlignment: "center",
+  padding: 1,
+  margin: 1,
+  borderStyle: "singleDouble"
+});
+
+// Round (rounded corners)
+const roundBox = boxen(message, {
+  title: title,
+  titleAlignment: "center",
+  padding: 1,
+  margin: 1,
+  borderStyle: "round"
+});
+
+// Print all boxes
+console.log(classicBox);
+console.log(singleDoubleBox);
+console.log(roundBox);
